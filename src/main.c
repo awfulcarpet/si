471e5c6d80650753dd018ff192b3052d987be5f8
afwulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2024-12-26 14:12:46 -0700

    draw line on image to map on screen

--- src/main.c
+++ src/main.c
@@ -1,11 +1,15 @@
 #include <X11/X.h>
 #include <X11/Xlib.h>
+#include <X11/Xutil.h>
 #include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
 
 Display *dpy;
 Window w;
 GC gc;
 XEvent e;
+int scr;
 
 void
 init_client(int width, int height)
@@ -23,6 +27,7 @@ init_client(int width, int height)
 	gc = XCreateGC(dpy, w, 0, NULL);
 	XSetForeground(dpy, gc, white);
 
+	scr = DefaultScreen(dpy);
 
 	XMapWindow(dpy, w);
 
@@ -34,11 +39,21 @@ main(void)
 {
 	init_client(200, 100);
 
-	XDrawLine(dpy, w, gc, 10, 60, 180, 20);
-	XFlush(dpy);
+	XImage *img = XCreateImage(dpy, CopyFromParent, DefaultDepth(dpy, scr), ZPixmap, 0, NULL, 100, 100, 32, 0);
+	img->data = calloc(img->bytes_per_line * img->height, 1);
+	XInitImage(img);
 
+	for (int i = 0; i < 100; i++) {
+		XPutPixel(img, i, 25, 0xFFFF00);
+	}
+
+	XPutImage(dpy, w, gc, img, 0, 0, 0, 0, 100, 50);
+
+
+	XSync(dpy, False);
 	while (XNextEvent(dpy, &e) == 0);
 
+	XDestroyImage(img);
 	XUnmapWindow(dpy, w);
 	XDestroyWindow(dpy, w);
 	XCloseDisplay(dpy);
